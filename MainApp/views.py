df6660cd356e5080489b45ee3ebaa310c8825a95
Yuri <zayycev22@gmail.com>
2020-05-28 14:56:14 +0300

    login, user info

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
@@ -5,6 +6,7 @@ from django.contrib import auth
 from django.http import HttpResponse, HttpResponseNotFound
 from .forms import New_User, New_Wallet
 from .models import Wallet
+from django.http import Http404
 
 
 def index(request):
@@ -19,7 +21,8 @@ def register(request):
         name = request.get('first_name')
         surname = request.get('last_name')
         new_wal = request.get('wallet')
-        user = User(username=username, password=make_password(password),email=email,first_name=name,last_name=surname)
+        user = User(username=username, password=make_password(password), email=email, first_name=name,
+                    last_name=surname)
         user.save()
         wallet = Wallet(wallet=new_wal, user_id=user.id)
         wallet.save()
@@ -27,4 +30,34 @@ def register(request):
 
     return render(request, 'register.html')
 
+
+def login_page(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        print(username, password, "try to log in")
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+
+        else:
+            print("Auth problems ...")
+        return redirect("/")
+
+    else:
+        print("HUGE PROBLEM")
+        raise Http404
+
+    print(username, "logged in")
+    return redirect('/')
+
+
+@login_required()
+def user_status(request):
+    u = request.user.pk
+    info = User.objects.get(id=u)
+    wallet = Wallet.objects.get(user_id=u)
+    context = {'info': info, 'wallet': wallet}
+    return render(request, "info.html", context)
 
