c17448eac65f687d1e3137bab1ed9b022590847a
Yuri <zayycev22@gmail.com>
2020-05-28 14:39:20 +0300

    ернул как было

--- MainApp/views.py
+++ MainApp/views.py
@@ -13,26 +13,18 @@ def index(request):
 
 def register(request):
     if request.method == 'POST':
-        wallet = New_Wallet(request.POST)
-        form = New_User(request.POST)
-        if form.is_valid() and wallet.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            email = form.cleaned_data.get('email')
-            name = form.cleaned_data.get('first_name')
-            surname = form.cleaned_data.get('last_name')
-            new_wal = wallet.cleaned_data.get('wallet')
-            user = User(username=username,password=make_password(password),email=email,first_name=name,last_name=surname)
-            user.save()
-            new_wallet = Wallet(wallet=new_wal,user_id=user.id)
-            new_wallet.save()
-            auth.login(request, user)
+        username = request.get('username')
+        password = request.get('password')
+        email = request.get('email')
+        name = request.get('first_name')
+        surname = request.get('last_name')
+        new_wal = request.get('wallet')
+        user = User(username=username, password=make_password(password),email=email,first_name=name,last_name=surname)
+        user.save()
+        wallet = Wallet(wallet=new_wal, user_id=user.id)
+        wallet.save()
+        auth.login(request, user)
 
-    else:
-        wallet = New_Wallet
-        form = New_User
-
-    context = {'form': form, 'wallet': wallet}
-    return render(request, 'register.html', context)
+    return render(request, 'register.html')
 
 
