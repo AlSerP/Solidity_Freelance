aa9e7b6cafb2e3061fa633c6fda06e76a43fcf91
Yuri <zayycev22@gmail.com>
2020-05-28 09:01:33 +0300

    ахуительные сложные модели

--- MainApp/models.py
+++ MainApp/models.py
@@ -2,3 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Wallet(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    wallet = models.CharField(max_length=1000)
