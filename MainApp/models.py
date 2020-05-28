8bd38586306214ff351bb91e888a861dcb28aeda
Yuri <zayycev22@gmail.com>
2020-05-28 15:59:14 +0300

    Task

--- MainApp/models.py
+++ MainApp/models.py
@@ -4,4 +4,11 @@ from django.contrib.auth.models import User
 
 class Wallet(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    wallet = models.CharField(max_length=1000)
+    wallet = models.CharField(max_length=1000, unique=True)
+
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    describe = models.TextField()
+    cost = models.IntegerField()
